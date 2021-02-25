fbb62c38567096d434b6bf3e3abe492b02cf43a8
JacobGraham02 <jakegraham54@gmail.com>
2021-02-25 15:54:05 -0500

    Added typewriter-like text which displays my personality. Added website content. Added better compatability with mobile devices. Added a picture of myself.

--- test/index.php
+++ test/index.php
@@ -7,11 +7,10 @@
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <meta name="description" content="A nicely-built portfolio displaying my personality, skills, and various projects I have built over the years"/>
             <meta name="keywords" content="Portfolio, Projects, About Me, Skills, Software Development, HTML5, CSS3, JavaScript, Java, PHP"/>
-            <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-            <meta content="utf-8" http-equiv="encoding">
+            <meta content="text/html;charset=utf-8" http-equiv="Content-Type"/>
+            <meta content="utf-8" http-equiv="encoding"/>
 
-            <link rel="stylesheet" href="resources/styles/homePage.css"/>
-            <link rel="stylesheet" href="resources/styles/hamburgerMenu.css"/>         
+            <link rel="stylesheet" href="resources/styles/homePage.css"/>    
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
             <!-- <link rel="shortcut icon" href="" type=image/x-icon/> -->
 
@@ -20,47 +19,61 @@
 
         <body>
             <header id="header" class="header">
+
+                <!-- <div id="logoFigureContainer">
+                    <figure id="logoFigure">
+                        <img src="resources/images/placeholder.png" alt="A placeholder image" title="A placeholder image">
+                    </figure>
+                </div> -->
+
                     <input class="menu-btn" type="checkbox" id="menu-btn" />
                     <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
 
                     <ul class="menu">
-                        <li><a href="#" data-section="#home"><i class=""></i>Home</a></li>
-                        <li><a href="#" data-section="#about"><i class=""></i>About</a></li>
-                        <li><a href="#" data-section="#projects"><i class=""></i>Projects</a></li>
-                        <li><a href="#" data-section="#skills"><i class=""></i>Skills</a></li>
+                        <li><a href="#" data-section="#home"><i class="" title="Go to 'home' section"></i><span class="underline">Home</span></a></li>
+                        <li><a href="#" data-section="#about"><i class="" title="Go to 'about' section"></i><span class="underline">About</span></a></li>
+                        <li><a href="#" data-section="#projects"><i class="" title="Go to 'projects' section"></i><span class="underline">Projects</span></a></li>
+                        <li><a href="#" data-section="#skills"><i class="" title="Go to 'skills' section"></i><span class="underline">Skills</span></a></li>
                     </ul>
           
             </header>
 
             <main id="main">
-                <section>
-                    <h2>This is main section</h2>
-                        <figure>
-                            <a href="https://www.linkedin.com/in/jacob-graham02/" class="fa fa-linkedin"></a>
+                <section id="home" class="c">
+                    <div id="personal_info">
+                         <figure id="personalPictureFigure">
+                            <img src="resources/images/personal_image.png" alt="A picture of me" id="pictureOfMe"/>
                         </figure>
-    
+                    </div>
+
+                    <div id="container">
+                        <div id="text"></div>
+                    </div>
+
+                    <div id="social_media">
                         <figure>
-                            <a href="https://github.com/JacobGraham02" class="fa fa-github"></a>
+                            <a href="https://www.linkedin.com/in/jacob-graham02/" class="fa fa-linkedin link-icons"></a>
                         </figure>
-    
+        
                         <figure>
-                            <a href="" class="fa fa-envelope"></a>
+                            <a href="https://github.com/JacobGraham02" class="fa fa-github link-icons"></a>
                         </figure>
+        
+                        <figure>
+                             <a href="" class="fa fa-envelope link-icons"></a>
+                        </figure>
+                    </div>
                 </section>
 
-                <section id="home">
-                    Home
-                </section>
-
-                <section id="about">
+                <section id="about" class="c">
                     About
                 </section>
 
-                <section id="projects">
+                <section id="projects" class="c">
                     Projects
                 </section>
 
-                <section id="skills">
+                <section id="skills" class="c">
                     Skills
                 </section>
 
--- test/resources/scripts/homePage.js
+++ test/resources/scripts/homePage.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const menu_links = document.querySelectorAll('.menu a');
+
+    menu_links.forEach(link => link.addEventListener("click", function() {
+        document.querySelector(".menu-btn").checked = false;
+    }))
+
     // JQuery's addEventListener()
     $("a").on("click", function() {
         var id=$(this).data("section"); // Reference the current anchor object; 'this'
@@ -7,4 +13,64 @@ document.addEventListener("DOMContentLoaded", function() {
             $(id).fadeIn(250);
         });
     });
+
+    // Collapse menu if a link is selected
+    if (window.innerWidth <= 768) {
+        $('.menu a').on("click", function() {
+            $('.menu-btn').prop('checked', false);
+        });
+    }
+
+    // For the typewriter-like text
+
+    // Sentences
+    var _CONTENT = ["Hello there","How are you","I am good, you?","I am doing very well, thank you."];
+    // Current sentence being processed
+    var _PART = 0;
+    // Character number of the current sentence being processed
+    var _PART_INDEX = 0;
+
+    var _INTERVAL_VAL;
+
+    var _ELEMENT = document.querySelector("#text");
+
+    // Implement typing effect
+    function Type() {
+        var text = _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+        _ELEMENT.innerHTML = text;
+        _PART_INDEX++;
+
+        // If full sentence has been displayed, then start to delete the sentence after some time
+        if (text === _CONTENT[_PART]) {
+            clearInterval(_INTERVAL_VAL);
+            setTimeout(function() {
+                _INTERVAL_VAL = setInterval(Delete, 50);
+            }, 1000);
+        }
+    }
+
+    // Implements deleting effect
+    function Delete() {
+        var text = _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+        _ELEMENT.innerHTML = text;
+        _PART_INDEX--;
+
+        // If sentence has been deleted then start to display the next sentence
+        if (text === '') {
+            clearInterval(_INTERVAL_VAL);
+
+            if (_PART == (_CONTENT.length - 1)) 
+                _PART = 0;
+            else 
+                _PART++;
+            _PART_INDEX = 0;
+
+            // Start to display the next sentence after some time
+            setTimeout(function() {
+                _INTERVAL_VAL = setInterval(Type, 100);
+            }, 200);
+        }
+    }
+    // Start the typing effect on load
+    _INTERVAL_VAL = setInterval(Type, 100);
 });
